5d95a3aaca0395e8e928ef9410bbd2ade6a7585e
root <root@localhost.localdomain>
2020-01-03 15:56:20 +0000

    small app for testing socket connection

--- Program.cs
+++ Program.cs
@@ -8,47 +8,17 @@ namespace testsocketclient
 {
     class Program
     {
-        private const string IpString = "127.000.000.001";
-        private const int Port = 8888;
-
         static void Main(string[] args)
         {
-            Console.Title = "Socket Client";
-            Console.Write("Enter the IP of the server: ");
-        
-            IPAddress clientIP = IPAddress.Parse(IpString);
-            String message = String.Empty;
-
-            while (true)
-            {
-                message = "alo pozega";
-
-                IPEndPoint clientEP = new IPEndPoint(clientIP, Port);
-
-                // Setup the socket
-                Socket clientSock = new Socket(
-                    AddressFamily.InterNetwork,
-                    SocketType.Stream,
-                    ProtocolType.Tcp);
-
-                // Attempt to establish a connection to the server
-                Console.Write("Establishing connection to the server... ");
-                try
-                {
-                    clientSock.Connect(clientEP);
-
-                    // Send the message
-                    clientSock.Send(Encoding.UTF8.GetBytes(message));
-                    clientSock.Shutdown(SocketShutdown.Both);
-                    clientSock.Close();
-                    Console.Write("Message sent successfully.\n\n");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+            string input = string.Empty;
+            do{
+                Console.Write("Enter command, type 'q' to quit: ");
+                input = Console.ReadLine();
+                SynchronousSocketClient.StartClient(input);  
                 
-            }
+            }while(input != "q");
         }
+
+       
     }
 }
